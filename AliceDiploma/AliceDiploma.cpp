3e1859cddd050a8e3ef67393d2f2cca18b1e7222
tominokers <golmagrad@gmail.com>
2013-01-07 21:28:37 +0200

    Adds comments to this shit.

--- AliceDiploma/AliceDiploma.cpp
+++ AliceDiploma/AliceDiploma.cpp
@@ -28,24 +28,37 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
+
+	// загрузили союзы-разделители сегментов.
 	un_pattterns=create_patterns_map(un_set);
+	// загрузили базу лемм.
+	LemmOrganizer lo("words.txt");
+
 	cout<<"Введите предложение \n";
-	char sentence[1000];
-	cin.getline(sentence,1000);
+	const int powerOfTwo = 1024;
+	char sentence[powerOfTwo];
+	cin.getline(sentence, powerOfTwo);
 	int len=strlen(sentence);
 	for(int i=0;i<strlen(sentence);++i)
 	{
 		sentence[i]=tolower(sentence[i],locale("Russian"));
 	}
-	LemmOrganizer lo("words.txt");
-	vector<Word> res=divide(sentence, additional_patterns, &lo);
+
+	// разбиваем предложение на слова.
+	vector<Word> res = divide(sentence, additional_patterns, &lo);
+	// какая-то странная магия с союзами.
 	set_unions(res,un_set,un_pattterns);
+
+	// тут типа как получаем предложение в виде сегментов.
+	// и я так понимаю оно должно быть заполнено МИ.
 	vector<Segment> segments=get_segments(res);
 	for(vector<Segment>::iterator it=segments.begin();it!=segments.end();++it)
 	{
 		setSegment(*it);
-		//if()
 	}
+	// дебаг показывает, что в структуре много чего не иницилизировано.
+	// да там вообще все наполовину мертво! *wall*
+
 	for(vector<Segment>::iterator it=segments.begin();it!=segments.end();++it){
 		cout<<it->m_type;
 		for(vector<Word>::iterator it2=it->m_left_border;it2!=it->m_right_border;++it2)
